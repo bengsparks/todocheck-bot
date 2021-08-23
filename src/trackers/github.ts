febd03346ea08e953802300d64aa411b96ad7481
Benjamin Sparks <benjamin.sparks@protonmail.com>
2021-08-24 00:25:14 +0200

    Implement reopening of issues

--- src/trackers/github.ts
+++ src/trackers/github.ts
@@ -4,27 +4,23 @@ import { GitHub } from "@actions/github/lib/utils";
 export const tag = "github";
 
 class GithubTracker implements Tracker {
-    constructor(private octokit: InstanceType<typeof GitHub>, private owner: string, private repo: string) {
+    constructor(private octokit: InstanceType<typeof GitHub>, private metadata: { owner: string, repo: string }) {
     }
 
     async getIssue(issueId: string): Promise<Issue> {
-        return this.octokit.request("GET /repos/{owner}/{repo}/issues/{issueId}", {
-            owner: this.owner,
-            repo: this.repo,
-            issueId: issueId
+        return this.octokit.request("GET /repos/{owner}/{repo}/issues/{issueNumber}", {
+            ...this.metadata,
+            issueNumber: issueId
         }).then((response) => {
             if (response.status !== 200) {
                 return Promise.reject(response)
             }
 
-            return Promise.resolve({
-                isOpen: response.data.state === "open",
-                issueId: issueId
-            })
+            return Promise.resolve(makeIssueFromGithub(response.data))
         });
     }
 
-    async getIssues(issueIds: string | [string]): Promise<Issue[]> {
+    async getIssues(issueIds: string | string[]): Promise<Issue[]> {
         if (typeof issueIds === "string") {
             return this.getIssue(issueIds).then((issue) => {
                 return Promise.resolve([issue]);
@@ -32,8 +28,7 @@ class GithubTracker implements Tracker {
         }
 
         return this.octokit.request("GET /repos/{owner}/{repo}/issues", {
-            owner: this.owner,
-            repo: this.repo,
+            ...this.metadata
         }).then((response) => {
             if (response.status !== 200) {
                 return Promise.reject(response);
@@ -47,28 +42,52 @@ class GithubTracker implements Tracker {
                 return Promise.reject(response);
             }
 
-            return Promise.resolve(matchingIssues.map((githubIssue) => {
-                return {
-                    isOpen: githubIssue.state === "open",
-                    issueId: githubIssue.id.toString(10)
-                }
-            }))
+            return Promise.resolve(matchingIssues.map(makeIssueFromGithub));
         });
+    }
+
+    async reopenIssue(issue: Issue): Promise<Issue> {
+        return this.octokit.request("PATCH /repos/{owner}/{repo}/issues/{issueNumber}", {
+            ...this.metadata,
+            issueNumber: issue.issueId,
+            state: "open"
+        }).then((response) => {
+            if (response.status !== 200) {
+                return Promise.reject(response);
+            }
+
+            return Promise.resolve(makeIssueFromGithub(response.data))
+        });
+    }
+
+
+    async reopenIssues(issues: Issue | Issue[]): Promise<Issue[]> {
+        // Github REST Api does not seem to support multiple issue updates, 
+        // so issue all requests and Promise.all 
+        const allIssues = !Array.isArray(issues) ? [issues] : issues;
+        return Promise.all(allIssues.map(this.reopenIssue));
     }
 }
 
 
 export const initGithubTracker = async (input: { token: string }): Promise<GithubTracker> => {
     return new Promise<GithubTracker>(() => {
-        const octokit = getOctokit(input.token);
         const repoEnvVar = process.env.GITHUB_REPOSITORY;
-
         if (repoEnvVar === undefined) {
             return Promise.reject("GITHUB_REPOSITORY environment variable must be set!")
         }
 
         const [owner, repo] = repoEnvVar.split("/")
+        const octokit = getOctokit(input.token);
 
-        return new GithubTracker(octokit, owner, repo);
+        return new GithubTracker(octokit, { owner, repo });
     });
+}
+
+
+const makeIssueFromGithub = (githubIssue: { state: string, id: number }): Issue => {
+    return {
+        isOpen: githubIssue.state === "open",
+        issueId: githubIssue.id.toString(10)
+    }
 }
--- src/trackers/tracker.ts
+++ src/trackers/tracker.ts
@@ -1,6 +1,9 @@
 interface Tracker {
     getIssue(issueId: string): Promise<Issue>;
-    getIssues(issueIds: string | [string]): Promise<Issue[]>
+    getIssues(issueIds: string | string[]): Promise<Issue[]>
+
+    reopenIssue(issue: Issue): Promise<Issue>
+    reopenIssues(issues: Issue | Issue[]): Promise<Issue[]>
 }
 
 type Issue = {
