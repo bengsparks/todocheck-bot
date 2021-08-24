822b1e99b25be94db52f05c985f4a349bb0e638e
Benjamin Sparks <benjamin.sparks@protonmail.com>
2021-08-24 18:49:47 +0200

    Unify creation of referenced issue and tracker

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,20 @@ import *  as github from "./trackers/github";
 
 
 const main = async () => {
-    const inputs: Inputs = (() => {
-        const i = process.env.GITHUB_REPOSITORY ? github.readInputsFromAction() : undefined;
-        if (!i) {
+    const [inputs, tracker] = (() => {
+        let i: Inputs;
+        let t: Tracker;
+
+        if (process.env.GITHUB_REPOSITORY) {
+            i = github.readInputsFromAction();
+            t = github.initGithubTracker(i);
+        }
+
+        else {
             throw new Error("Unknown host!")
         }
-        return i;
-    })();
 
-
-    const tracker = ((): Tracker => {
-        const t = process.env.GITHUB_REPOSITORY ? github.initGithubTracker(inputs) : undefined;
-        if (!t) {
-            throw new Error("Unknown host!")
-        }
-        return t;
+        return [i, t] as const;
     })();
 
     // TODO: Support closing of multiple issues, e.g. when a Pull Request is merged
